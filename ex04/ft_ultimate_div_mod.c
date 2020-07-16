df768c298bf381c882b2abd660282a8b1e55a826
Dumisani Sithole <dusithol@c5r6s1.wethinkcode.co.za>
2020-07-16 13:41:39 +0200

    ex04

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,16 +6,17 @@
 /*   By: dusithol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 12:05:12 by dusithol          #+#    #+#             */
-/*   Updated: 2020/07/14 12:21:53 by dusithol         ###   ########.fr       */
+/*   Updated: 2020/07/16 13:41:13 by dusithol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int t = *a;
-	*a = *a / *b;
-	*b = t % *b;
+	int t;
+   		t= *a;
+		*a = *a / *b;
+		*b = t % *b;
 }
 
 
