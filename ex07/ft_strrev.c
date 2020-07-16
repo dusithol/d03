f1c13dc3c8459dcaf5a9c4d4e72bb650a065a31e
Dumisani Sithole <dusithol@c5r6s1.wethinkcode.co.za>
2020-07-16 12:43:05 +0200

    ex07

--- ex07/ft_strrev.c
+++ ex07/ft_strrev.c
@@ -6,19 +6,19 @@
 /*   By: dusithol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 13:45:04 by dusithol          #+#    #+#             */
-/*   Updated: 2020/07/15 16:05:39 by dusithol         ###   ########.fr       */
+/*   Updated: 2020/07/15 16:45:15 by dusithol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-
-char	ft_strrev(char *str)
+char*	ft_strrev(char *str)
 {
 	int size = 0;
 		int i = 0;
-	char *rev;
+	char *rev = (char *)malloc(sizeof(char) * 13);
 	
 
 	while(str[size] != '\0')
@@ -33,19 +33,19 @@ char	ft_strrev(char *str)
 
 				}
 				
-				str = rev;
-				return (*str);
+		//		str = rev;
+				return (rev);
 }
 
 int 	main()
 {
 	char *str = "Dumisani Sithole!";
 	
-	char x = 0;
+//	char *x; //= 0;
 	
 
-	x = ft_strrev(str);
-	printf("reverse is %d\n", x);
+	char *x = ft_strrev(str);
+	printf("reverse is %s\n", x);
 
 	return (0);
 }
